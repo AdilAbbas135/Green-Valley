5001204aa09f07f22f272ea19548eb46edde32d8
Adil Abbas <adilabbas8092@gmail.com>
2023-07-18 00:55:31 -0700

    changes done

--- Routes/find.js
+++ Routes/find.js
@@ -4,10 +4,26 @@ const ProductsModel = require("../Model/Products");
 
 const router = express.Router();
 
+//FIND ALL PRODUCTS FOR PLANT STORE PAGE
 router.get("/products", async (req, res) => {
   try {
     const Products = await ProductsModel.find();
     return res.status(200).json({ Products });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//FETCH SINGLE PRODUCT DETAILS
+router.get("/products/:id", async (req, res) => {
+  try {
+    const Product = await ProductsModel.findById(req.params.id);
+    if (Product) {
+      return res.status(200).json({ Product: Product });
+    } else {
+      return res.status(404).json({ error: "Product Not Found" });
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Internal Server Error" });
